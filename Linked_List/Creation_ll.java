5113537f06a3dfb0c1c13deacd72ac4d6b978ecf
Aman Kumar <aman.tech1011@gmail.com>
2026-06-23 09:34:05 +0530

    iterative search

--- Linked_List/Creation_ll.java
+++ Linked_List/Creation_ll.java
@@ -13,7 +13,7 @@ public class Creation_ll {
 
     public static Node head;
     public static Node tail;
-
+    public static int size;
     // creating Node at first
     public static void atFirst(int data) {
         Node newNode = new Node(data);
@@ -49,6 +49,31 @@ public class Creation_ll {
        temp.next=newNode;
 
     }
+//-----------------------------------------------removal-------------------------------
+    public static int removeAtFirst(){
+        if(head==null){
+            System.out.println("ll is empty");
+            return -1;
+        }
+       int temp=head.data;
+       head=head.next;
+       return temp;
+    }   
+ //----------------------------------------------------------------------------------
+//calculate yhe size of ll
+    public static void size(){
+        if(head==null){
+            System.out.println("linked list is empty");
+            return;
+        }
+       // int size=0;
+        Node temp=head;
+        while(temp!=null){
+            temp=temp.next;
+            size++;
+        }
+        System.out.println("linked list size is: "+size);
+    }     
     // display the linked list
     public static void display(){
         Node temp=head;
@@ -60,6 +85,21 @@ public class Creation_ll {
             temp=temp.next;
         }
     }
+    //------------iterative search------------------
+    public static int iterativeSearch(int key) {
+    Node temp = head;
+    int index = 0;
+
+    while (temp != null) {
+        if (temp.data == key) {
+            return index;
+        }
+        temp = temp.next;
+        index++;
+    }
+
+    return -1;
+}
     public static void main(String[] args) {
         Creation_ll ll = new Creation_ll();
         ll.atFirst(3);
@@ -68,7 +108,11 @@ public class Creation_ll {
         ll.atLast(6);
         ll.atFirst(5);
         ll.atMiddle(9, 2);
+        ll.removeAtFirst();
+        ll.removeAtFirst();
         ll.display();
+        ll.size();
+        System.out.println(ll.iterativeSearch(4));
 
     }
 }
