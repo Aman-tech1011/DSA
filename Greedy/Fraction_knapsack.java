ecc70d5c8edaf8610b3c216bdfcac1ac77c63f56
Aman Kumar <aman.tech1011@gmail.com>
2026-07-11 09:41:09 +0530

    tree traversL

---
+++ Greedy/Fraction_knapsack.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Fraction_knapsack{
+    public static void main(String[] args) {
+        int value[]={60,100,120};
+        int weight[]={10,20,30};
+        int total=50;
+         int capacity=total;
+         double ratio[][]=new double[value.length][2];
+        for(int i=0; i<value.length; i++){
+         ratio[i][0]=i;
+         ratio[i][1]=value[i]/(double)weight[i];          
+        }
+        Arrays.sort(ratio,Comparator.comparingDouble(o->o[1]));
+        int finalValue=0;
+        for(int i=ratio.length-1; i>=0; i--){
+             int idx=(int)ratio[i][0];
+             if(capacity >= weight[idx]){
+                finalValue+=value[idx];
+                capacity-=weight[idx];
+             }else{
+                finalValue+=(ratio[i][1]*capacity);
+                capacity=0;
+                break;
+             }
+        }
+        System.out.println("Final value : "+finalValue);
+    }
+}
